7c93b3025b561871575acb7081e5d29245d1e309
samuelsicon <33673659+samuelsicon@users.noreply.github.com>
2018-06-28 22:21:51 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    //select input
+    let height, width, color;
+    //Let user choose size of the grid
+    $('#sizePicker').submit(function(event) {
+      event.preventDefault();
+      height = $('#inputHeight').val();
+      width = $('#inputWeight').val();
+      makeGrid(height, width);
+    });
+    //Make grid when user inputs size
+    function makeGrid(row, column) {
+      $('tbody').remove();
+      $('#pixelCanvas').append('<tbody></tbody>');
+      for (let row = 1; row <= height; row++) {
+        $('tbody').append('<tr></tr>');
+        for (let column = 1; column <= width; column++) {
+          $('tr:last').append('<td></td>');
+        }
+      }
+      //Let user choose color
+      $('td').click(function(event){
+        $( this ).css( "background-color", color );
+      });
+    }
+    //Let user select color
+    $('#colorPicker').change(function(event){
+      color = $('#colorPicker').val();
+    });
+});
+
+
